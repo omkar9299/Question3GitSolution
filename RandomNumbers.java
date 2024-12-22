88e871e2240ac657f32a9c59c270860811cf2e2e
omkar9299 <omsalvi9299@gmail.com>
2024-12-22 14:40:30 +0530

    RandomNumbers.java

--- RandomNumbers.java
+++ RandomNumbers.java
@@ -3,13 +3,13 @@ import java.util.Random;
 
 public class RandomNumbers {
     public static void main(String[] args) {
-        // Create an instance of Random class
+        
         Random rand = new Random();
 
-        // Loop to generate and print five 4-digit random numbers
+       
         for (int i = 0; i < 5; i++) {
-            // Generate a random 4-digit number between 1000 and 9999
-            int randomNumber = 1000 + rand.nextInt(9000); // 9000 is the range, plus 1000 ensures 4 digits
+           
+            int randomNumber = 1000 + rand.nextInt(9000); 
             System.out.println(randomNumber);
         }
     }
